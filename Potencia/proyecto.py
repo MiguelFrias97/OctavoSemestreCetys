8a9c617c35826e347baaaad9ac8b1b0f710c0f8b
MiguelFrias <miguel.frias@cetys.edu.mx>
2019-03-24 14:20:10 -0700

    Refinando funcion mail

--- Potencia/proyecto.py
+++ Potencia/proyecto.py
@@ -1,13 +1,12 @@
 import smtplib
+
 def sendMail(sender,to,subject,body):
-	message =  """\
-	From: %s
-	To: %s
-	Subject: %s
+	message =  """From: %s
+To: %s
+Subject: %s
 
-	%s
-
-	""" % (sender,", ".join(to) if len(to)>1 else to[0],subject,body)
+%s
+""" % (sender,', '.join(to) if len(to)>1 else to[0],subject,body)
 
 	try:
 		server = smtplib.SMTP_SSL('smtp.gmail.com',465)
@@ -19,7 +18,7 @@ def sendMail(sender,to,subject,body):
 
 if __name__=="__main__":
 	sender = 'seguridad.potencia.ice@gmail.com'
-	to = ['miguel.frias@cetys.edu.mx']
+	to = ['nataliab@cetys.edu.mx','ariana-landeros@cetys.edu.mx']
 	subject = 'Testing mail'
 	body = 'I am testing the mail function'
 
